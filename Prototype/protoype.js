12e6b4d0e915f2c49886a91add0c27e65abfd78b
ahmed <ag3204123@gmail.com>
2024-08-05 02:15:57 -0700

    prototype--2

--- Prototype/protoype.js
+++ Prototype/protoype.js
@@ -9,9 +9,18 @@ function User(name) {
 }
 
 console.log(User.prototype);
-
+const user1 = new User("ahmed");
 const arr = [1, 2, 3, 4];
 console.log(arr.constructor);
 
 // THE PROTOTYPE OF CONSTRUCTOR INHERTE TO ALL INSTANCES
 console.log(Array.prototype);
+
+console.log(user1);
+User.prototype.addTitle = () => `hello world`;
+
+console.log(user1.addTitle());
+Object.prototype.gamal = "ahmed gamal";
+
+const obj = { 1: 2 };
+console.log(obj.gamal);
